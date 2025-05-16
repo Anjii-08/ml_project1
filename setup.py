9d6dfdd34b7132228a441a6a730b6d1d8d9cab07
Anjaneyulu <baikanianji@gmail.com>
2025-05-17 00:42:20 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
-print("hello world")
+from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+
+    with open(file_path,"r") as f:
+        requirements=f.readlines()
+    
+    requirements=[req.replace("\n","") for req in requirements]
+
+    return requirements
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anjaneyulu",
+    author_email="baikanianji@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
